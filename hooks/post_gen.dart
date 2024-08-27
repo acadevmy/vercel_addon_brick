fd897e43915948d9422e8294de677700d4388a9d
Iacopo Ciao <iacopociao1992@gmail.com>
2024-08-27 13:02:12 +0200

    fix: fix vault

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -29,46 +29,25 @@ void run(HookContext context) {
     appendToGitlab(gitlabConfiguration, context);
   }
 
-  context.logger
-      .info('Remember to configure the following variables on .env.vault:');
-
-  final vault = new Vault(
-    Directory.fromUri(
-      Directory.current.uri.resolve('../..'),
-    ),
-  );
+  final vault = new Vault(Directory.current.absolute.parent.parent);
 
   final Map<String, dynamic> rawVercelContext = context.vars[kVercelContextKey];
 
   final Map<Environment, VercelProject> vercelContext =
       deserializeVercelContext(rawVercelContext);
 
-  final applicationName = context.vars[kApplicationNameKey];
+  final applicationName = context.vars[kApplicationNameKey] as String;
 
   for (final entry in vercelContext.entries) {
     final envName = entry.key.name;
-    final projectIdVariableName = "${applicationName}_VERCEL_PROJECT_ID";
+    final projectIdVariableName =
+        "${applicationName.constantCase}_VERCEL_PROJECT_ID";
     final projectId = entry.value.projectId;
-    final orgIdVariableName = "${applicationName}_VERCEL_ORG_ID";
+    final orgIdVariableName = "${applicationName.constantCase}_VERCEL_ORG_ID";
     final orgId = entry.value.orgId;
-    final vercelTokenVariableName = "${applicationName}_VERCEL_TOKEN";
+    final vercelTokenVariableName =
+        "${applicationName.constantCase}_VERCEL_TOKEN";
     final vercelToken = entry.value.token;
-
-    context.logger.info(
-      '🔐 Configuring .env.vault for $envName',
-    );
-
-    context.logger.info(dolumnify(
-      [
-        ['VARIABLE', 'VALUE'],
-        [vercelTokenVariableName, vercelToken],
-        [projectIdVariableName, projectId],
-        [orgIdVariableName, orgId],
-      ],
-      columnSplitter: ' | ',
-      headerIncluded: true,
-      headerSeparator: '=',
-    ));
 
     vault
       ..pull(envName)
@@ -88,6 +67,26 @@ void run(HookContext context) {
         vercelToken,
       )
       ..push(envName);
+
+    context.logger.info(
+      '🔐 Configured .env.vault for $envName',
+    );
+
+    context.logger.info('\n');
+
+    context.logger.info(dolumnify(
+      [
+        ['VARIABLE', 'VALUE'],
+        [vercelTokenVariableName, vercelToken],
+        [projectIdVariableName, projectId],
+        [orgIdVariableName, orgId],
+      ],
+      columnSplitter: ' | ',
+      headerIncluded: true,
+      headerSeparator: '=',
+    ));
+
+    context.logger.info('\n\n');
   }
 }
 
--- hooks/utilities/vault.dart
+++ hooks/utilities/vault.dart
@@ -5,17 +5,17 @@ class Vault {
   Vault(this.directory);
 
   void pull(String environment) {
-    Process.run('pnpm', ['dlx', 'dotenv-vault', 'pull', environment],
+    Process.runSync('pnpm', ['dlx', 'dotenv-vault', 'pull', environment],
         workingDirectory: directory.path);
   }
 
   void push(String environment) {
-    Process.run('pnpm', ['dlx', 'dotenv-vault', 'push', environment],
+    Process.runSync('pnpm', ['dlx', 'dotenv-vault', 'push', environment],
         workingDirectory: directory.path);
   }
 
   void addVariable(String environment, String name, dynamic value) {
     final dotenv = File.fromUri(directory.uri.resolve('.env.$environment'));
-    dotenv.writeAsStringSync('$name=$value', mode: FileMode.append);
+    dotenv.writeAsStringSync('\n$name=$value', mode: FileMode.append);
   }
 }
