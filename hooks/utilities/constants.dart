9789f1218547bce0f976db0a482adfc842763f30
Iacopo Ciao <iacopociao1992@gmail.com>
2024-09-27 12:45:11 +0200

    chore: bump devmy devops version

--- hooks/utilities/constants.dart
+++ hooks/utilities/constants.dart
@@ -2,4 +2,4 @@ const kGitlabFileName = '.gitlab-ci.yml';
 const kApplicationNameKey = 'applicationName';
 const kVercelContextKey = 'vercel';
 const kVercelFrameworkKey = 'framework';
-const kVercelDevopsConfigVersion = 'v1.1.6';
+const kVercelDevopsConfigVersion = 'v1.2.4';
